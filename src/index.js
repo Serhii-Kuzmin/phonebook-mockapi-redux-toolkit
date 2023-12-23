b9c92ac6f5538fbc77bd681b77cba2e29c6ca8ac
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-23 10:27:47 +0100

    refactoring-bookSlice

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/redux/phoneBookSlice.js
+++ src/redux/phoneBookSlice.js
@@ -1,41 +1,86 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import {
+  delContactThunk,
+  getContactsThunk,
+  postContactThunk,
+} from 'services/fetchContacts';
 
 const contactInitialState = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
+  isLoading: false,
+  error: null,
 };
 
-const phoneBookSlice = createSlice({
+const onPending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const onRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+
+const arrOfActs = [getContactsThunk, postContactThunk, delContactThunk];
+
+const addStatusActs = status => arrOfActs.map(el => el[status]);
+
+export const phoneBookSlice = createSlice({
   name: 'phoneBook',
   initialState: contactInitialState,
-  reducers: {
-    addContact(state, action) {
-      state.contacts = [...state.contacts, action.payload];
-    },
-    delContact(state, action) {
-      state.contacts = state.contacts.filter(
-        contact => contact.id !== action.payload
-      );
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(getContactsThunk.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.contacts = payload;
+        state.error = null;
+      })
+      .addCase(postContactThunk.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.contacts = [...state.contacts, payload];
+        state.error = null;
+      })
+      .addCase(delContactThunk.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.contacts = state.contacts.filter(
+          contact => contact.id !== payload.id
+        );
+        state.error = null;
+      })
+      .addMatcher(isAnyOf(...addStatusActs('pending')), onPending)
+      .addMatcher(isAnyOf(...addStatusActs('rejected')), onRejected);
   },
 });
 
-export const getPhoneBookValue = state => state.phoneBook.contacts;
+export const getPhoneBookValue = state => state.phonebook.contacts;
+export const getIsLoading = state => state.phonebook.isLoading;
+export const getError = state => state.phonebook.error;
 
-export const { addContact, delContact } = phoneBookSlice.actions;
+// const phoneBookSlice = createSlice({
+//   name: 'phoneBook',
+//   initialState: contactInitialState,
+//   reducers: {
+//     addContact(state, action) {
+//       state.contacts = [...state.contacts, action.payload];
+//     },
+//     delContact(state, action) {
+//       state.contacts = state.contacts.filter(
+//         contact => contact.id !== action.payload
+//       );
+//     },
+//   },
+// });
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
+// export const getPhoneBookValue = state => state.phoneBook.contacts;
 
-export const contactsPersistReducer = persistReducer(
-  persistConfig,
-  phoneBookSlice.reducer
-);
+// export const { addContact, delContact } = phoneBookSlice.actions;
+
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
+
+// export const contactsPersistReducer = persistReducer(
+//   persistConfig,
+//   phoneBookSlice.reducer
+// );
